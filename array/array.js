f97431e1e127e017a2890fec2246a3d42192e366
Syed Mehar Ali Shah <syedzada12550@gmail.com>
2023-11-08 17:43:02 +0500

    add new  assigmnets

--- array/array.js
+++ array/array.js
@@ -12,6 +12,11 @@
 
 
 
+// ***************************** Not solve ***************************** 
+
+
+
+
 
 // ***************************** 3 ***************************** 
 
@@ -96,41 +101,208 @@
 
 
 
+// ***************************** 11 *****************************   
 
 
-// ***************************** 11 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
-// ***************************** 5 ***************************** 
+// let studentName = ["Syed", "Mehar", "Shah"];
+// let studentScore = [320, 230, 480];
+// let totalScore = 500;
+
+// for (let i = 0; i < studentScore.length; i++) {
+//     let name = studentName[i];
+//     let score = studentScore[i];
+//     let percentage = score / totalScore * 100;
+//     console.log(`Score of ${name} is ${score} and Percentage is : ${percentage} %`);
+// }
+
+
+
+// ***************************** 12 *****************************   
+
+// let color = ["Red", "Yellow", "Green", "Blue", "Orange"];
+// console.log(`Colors : ${color}`);
+// ***************************** a *****************************
+
+// let user1 = prompt("Add new Color in start");
+// color.unshift(user1);
+// console.log(color);
+// ***************************** b *****************************
+
+// let user2 = prompt("Add new Color in end");
+// color.push(user2);
+// console.log(color);
+// ***************************** c *****************************
+
+// let user3 = prompt("Add two Color in start");
+// color.unshift(user3);
+// console.log(color);
+// ***************************** d *****************************
+
+// let user4 = prompt("Delete The First Color in the array");
+// color.shift(user4);
+// console.log(color);
+// ***************************** e *****************************
+
+// let user5 = prompt("Delete The last Color in the array");
+// color.pop(user5);
+// console.log(color);
+// ***************************** f *****************************
+
+// let user6 = +prompt("which index he/she wants to add color(s)");
+// let user7 = prompt("add the color to desired position/index");
+// color.splice(user6, 0, user7);
+// console.log(color);
+// ***************************** g *****************************
+
+// let user8 = +prompt("which index he/she wants to delete color(s)");
+// let user9 = +prompt("how many colors he/she wants to delete");
+// color.splice(user8, user9);
+// console.log(color);
+
+
+
+
+
+
+
+
+// ***************************** 13 *****************************
+
+// let studentScore = [320, 230, 480, 120];
+// console.log(`Score of Students: ${studentScore}`);
+// let sortStudentScore = studentScore.sort();
+// console.log(`Ordered Scores of Students : ${sortStudentScore}`);
+
+
+
+
+
+
+
+
+
+
+
+
+// ***************************** 14 *****************************
+
+
+// let fruits = ["Strawberry", "Apple", "Orange", "Banana"];
+// console.log(`Fruits List : ${fruits}`);
+// let sortFruits = fruits.sort();
+// console.log(`Ordered list of Fruits : ${sortFruits}`);
+
+
+
+
+// ***************************** 15 *****************************
+
+
+// let cityNames = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar" ];
+// console.log(`Cities List:`);
+// console.log(`${cityNames}`);
+// let selectCityNames = cityNames.splice(2, 2);
+// console.log(`Selected Cities List:`);
+// console.log(`${selectCityNames}`); 
+
+
+
+
+// ***************************** 16 *****************************
+
+// let arr = ["This", "is", "my", "cat"];
+// console.log(`Array:`);
+// console.log(`${arr}`);
+// let arr2 = arr.join(" ")
+// console.log(`String`);
+// console.log(`${arr2}`);
+
+
+
+
+
+
+// ***************************** 17 *****************************
+
+// let values = [];
+// values.push("Keyboard");
+// values.push("Mouse");
+// values.push("Printer");
+// values.push("Moniter");
+// console.log(`Devices:`);
+// console.log(values);
+// console.log(`Out:`);
+// console.log(values.shift());
+// console.log(`Out:`);
+// console.log(values.shift());
+// console.log(`Out:`);
+// console.log(values.shift());
+// console.log(`Out:`);
+// console.log(values.shift());
+
+
+
+// ***************************** 18 *****************************
+
+
+
+
+
+// let values = [];
+// values.push("Keyboard");
+// values.push("Mouse");
+// values.push("Printer");
+// values.push("Moniter");
+// console.log(`Devices:`);
+// console.log(values);
+// console.log(`Out:`);
+// console.log(values.pop());
+// console.log(`Out:`);
+// console.log(values.pop());
+// console.log(`Out:`);
+// console.log(values.pop());
+// console.log(`Out:`);
+// console.log(values.pop());
+
+
+
+
+
+// ***************************** 19 *****************************
+
+// ***************************** not complete *****************************
+
+
+// let down = ["Apple", "Samsung", "Motorola", "Nokia"];
+// let dropDown = document.querySelector("#dropDown");
+// for (let i = 0; i < down.length; i++) {
+//     dropDown.innerHTML = `<option >${down[i]}</option>`
+// }
+
+// ***************************** 20 *****************************
+
+
+
+// let color = [["green", 1], ["yellow", 2], ["blue", 3], [" orange", 4]];
+// console.log(color[0]);
+// console.log(color[1]);
+// console.log(color[2]);
+// console.log(color[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+// ***************************** 21 *****************************
+
+
+// let matrix = [[0, 1, 2, 3],[1, 0, 1, 2],[2, 1, 0, 1]];
+// console.log("Matrix:");
+// console.log(matrix);
--- ifstatment/ifstatement.js
+++ ifstatment/ifstatement.js
@@ -187,7 +187,13 @@
 
 
 
-// *********************************************** Not Solve ***********************************************
+// Get the calendar year from the user
+// var year = +(prompt("Enter a year:"));
+//     if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+//         console.log(year + " is a leap year.");
+//     } else {
+//         console.log(year + " is not a leap year.");
+//     }
 
 
 
